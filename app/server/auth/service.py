f692a0cbdebc80d2ccaf26ac2347d998247bdba5
Prabakaran <prabakaranb2004@gmail.com>
2026-03-27 10:43:21 +0530

    RBAC added

--- app/server/auth/service.py
+++ app/server/auth/service.py
@@ -14,9 +14,9 @@ from app.server.schema import Employee, EmployeeRole
 
 load_dotenv()
 
-SECRET_KEY=os.getenv("SECRET_KEY", "change-this-secret-in-production")
-ALGORITHM=os.getenv("JWT_ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
+SECRET_KEY=os.getenv("SECRET_KEY")
+ALGORITHM=os.getenv("JWT_ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 pwd_context=CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme=OAuth2PasswordBearer(tokenUrl="/auth/token")
